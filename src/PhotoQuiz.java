1066c078966242d3deb809dae9ced212fad7aedf
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-11 16:58:59 -0700

    MY first swing, and photo quiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -3,7 +3,6 @@
  *    Level 1
  */
 
-
 import java.awt.Component;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,25 +17,27 @@ public class PhotoQuiz {
 
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
-		quizWindow.setVisible(true);
+		quizWindow.setVisible(true);component
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+			String url = "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj90ZWkm5jcAhXEIjQIHUGaCTwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.inc.com%2Fkaren-schrier%2F4-brilliant-ideas-you-should-steal-from-food-industry.html&psig=AOvVaw1ZqnKjwHCaTwE7XOqfgY36&ust=1531438532315896";
 		// 2. create a variable of type "Component" that will hold your image
-
+			Component component;
 		// 3. use the "createImage()" method below to initialize your Component
-
+			component = createImage(url);
 		// 4. add the image to the quiz window
-
+			quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+			quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+			String cor = JOptionPane.showInputDialog("Wat is te first thing you see???????????????");
 		// 7. print "CORRECT" if the user gave the right answer
-
+			if (cor == "food");
+				System.out.println("CORRECT");
 		// 8. print "INCORRECT" if the answer is wrong
-
+			else:
+				System.out.println("INCORRECT");
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
@@ -60,10 +61,6 @@ public class PhotoQuiz {
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	// *15. make something happen when mouse enters image
+	// (imageComponent.addMouseMotionListener())
 }
-
-
-
-
-
